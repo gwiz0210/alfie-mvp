99ecf4ba5bb3c140370395473b47c4e21faf4b9a
Mary Gansallo <marygans@microsoft.com>
2023-01-10 23:52:14 -0500

    adding fonts and relevant assets with the Home and details page

---
+++ screens/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Text, View,SafeAreaView, FlatList} from 'react-native';
+import { useState } from 'react';
+import {COLORS, NFTData} from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex:1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList
+          data={NFTData}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
