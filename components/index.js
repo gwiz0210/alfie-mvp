58d144dd3e5aa85572f4353d93c1488c0860b20d
Mary Gansallo <marygans@microsoft.com>
2023-01-12 18:31:47 -0500

    adding more layout features

--- components/index.js
+++ components/index.js
@@ -1,7 +1,22 @@
-import HomeHeader  from "./HomeHeader";
-import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, StartDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
 
 export {
-    HomeHeader, FocusedStatusBar, NFTCard
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  StartDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
 }
