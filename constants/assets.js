13dbc30e5bb8480be31a87759095e613787d1603
Mary Gansallo <marygans@microsoft.com>
2023-01-15 15:48:03 -0500

    updated images on landing

--- constants/assets.js
+++ constants/assets.js
@@ -5,13 +5,13 @@ import left from "../assets/icons/left.png";
 import search from "../assets/icons/search.png";
 
 import alfieLogo from "../assets/images/alfie-logo.png";
-import restaurant01 from "../assets/images/restaurant01.png";
-import restaurant02 from "../assets/images/restaurant02.jpeg";
-import restaurant03 from "../assets/images/restaurant03.jpeg";
-import restaurant04 from "../assets/images/restaurant04.jpeg";
-import restaurant05 from "../assets/images/restaurant05.jpeg";
-import restaurant06 from "../assets/images/restaurant06.jpeg";
-import restaurant07 from "../assets/images/restaurant07.jpeg";
+import restaurant01 from "../assets/images/restaurant01.jpeg";
+import restaurant02 from "../assets/images/restaurant02.jpg";
+import restaurant03 from "../assets/images/restaurant03.jpg";
+import restaurant04 from "../assets/images/restaurant04.jpg";
+import restaurant05 from "../assets/images/restaurant05.jpg";
+import restaurant06 from "../assets/images/restaurant06.jpg";
+import restaurant07 from "../assets/images/restaurant07.jpg";
 
 import person01 from "../assets/images/person01.png";
 import person02 from "../assets/images/person02.png";
